d0802e8e61b33a06050b584e684b3bb837c6580b
Unknown <ronincash@yahoo.com>
2018-11-21 12:50:09 -0500

    Uploading Python work files.

--- testString1.py
+++ testString1.py
@@ -10,3 +10,12 @@ multi = """It was the best of times.
 # It was the best of times.
 #   It was the worst of times.
 print(multi)
+
+  # Split the line into chunks, which are concatenated automatically by Python
+text = (
+    "%d little pigs come out, "
+    "or I'll %s, and I'll %s, "
+    "and I'll blow your %s down."
+    % (3, 'huff', 'puff', 'house'))
+
+print(text)
